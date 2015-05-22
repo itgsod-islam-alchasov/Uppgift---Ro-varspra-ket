3a116ce4ad00de1677feee50e575ed0fddcf56db
Islam Alchasov <islam.alchasov@elev.it-gymnasiet.se>
2015-05-22 16:15:56 +0200

    Rovarized is working, for now.

---
+++ Uppgift---Ro-varspra-ket-master/lib/rovarizer.rb
@@ -0,0 +1,19 @@
+def rovarize(cleartext: )
+  konsonanter=["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
+  string = ""
+  cleartext.each_char do |x|
+    rovarized = "#{x}o#{x}"
+    if konsonanter.include?x
+      string = string + "#{rovarized}"
+    else
+      string = string + x
+    end
+  end
+  return string
+end
+
+def derovarize(cipher:)
+  
+end
+
+p rovarize(cleartext: "The pirate said you like men, is that true?")
